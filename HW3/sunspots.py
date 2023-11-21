f88746f38aadafe7e6f10c92c830ceea88fb7a23
Shashank Chidige <shashankchidige@gmail.com>
2023-11-21 13:17:53 -0500

    Implemented sunspots program

--- HW3/sunspots.py
+++ HW3/sunspots.py
@@ -3,6 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # Read in the data
-data = pd.read_csv('sunspots.txt', sep='\t')
+data = pd.read_csv('sunspots.txt', sep='\t', header=None, names=['Month', 'Sunspots'],index_col=None)
 
+print(data.head())
+month = data['Month']
+sunspots = data['Sunspots']
 
+month = data['Month']
+sunspots = data['Sunspots']
+
+# Plot the data
+plt.plot(month,sunspots)
+plt.title("Number of Sunspots over Time")
+plt.xlabel("Months since Jan 1749")
+plt.ylabel("Sunspot Number")
+plt.show()
